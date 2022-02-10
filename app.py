e999ef03c52122dcaa8a21bf6ffcd48d38e5e583
migbash <20924663+migbash@users.noreply.github.com>
2022-02-10 00:14:25 +0000

    updating main Flask-App

--- app.py
+++ app.py
@@ -4,6 +4,7 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # ~ OTHER LIBRARIES FOR PYTHON ~~~~~~~
 
+import os
 import json
 import requests
 import random
@@ -87,4 +88,6 @@ def post_question():
 
 if __name__ == "__main__":
     # ... start the application
-    app.run(debug=True, threaded=True, host='0.0.0.0', port=9000)
+    # app.run(debug=True, threaded=True, host='0.0.0.0', port=9000)
+    # app.run(threaded=True, host='0.0.0.0')
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
